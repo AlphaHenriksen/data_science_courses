4a74b8df177cfc853754bf07eb5f228266e340f9
Anders <anders-10@live.dk>
2022-05-02 10:45:39 +0200

    general cleanup of the other script

--- master_requirements/master_courses.py
+++ master_requirements/master_courses.py
@@ -34,9 +34,9 @@ timetable_to_time = {'January': "January",
 def ects_requirements(current_courses, general_1, general_2, techno):
     """Undersøger om der er taget for få eller for mange kurser i teknologiske linjefag eller generelle kompetencer"""
 
-    general_1_commons = pd.merge(current_courses, general_1["title"], how ='inner')
-    general_2_commons = pd.merge(current_courses, general_2["title"], how ='inner')
-    techno_commons = pd.merge(current_courses, techno["title"], how ='inner')
+    general_1_commons = pd.merge(current_courses, general_1["title"], how='inner')
+    general_2_commons = pd.merge(current_courses, general_2["title"], how='inner')
+    techno_commons = pd.merge(current_courses, techno["title"], how='inner')
     names = ["entrepreneurship", "general competance", "technological specialization", "thesis"]
 
     print(f"Total courses taken from {names[0]}: ")
@@ -47,7 +47,10 @@ def ects_requirements(current_courses, general_1, general_2, techno):
     print(techno_commons, "\n\n")
     print(f"Total courses taken from {names[3]}: ")
     print(current_courses[current_courses.title == "Master's Thesis"], "\n\n")
-    totals = [sum(general_1_commons["points"]), sum(general_2_commons["points"]), sum(techno_commons["points"]), current_courses.points[current_courses.title == "Master's Thesis"].values[0]]
+    totals = [sum(general_1_commons["points"]), 
+              sum(general_2_commons["points"]), 
+              sum(techno_commons["points"]), 
+              current_courses.points[current_courses.title == "Master's Thesis"].values[0]]
     neededs = [10, 20, 30, 30]
 
     for (name, total) in zip(names, totals):
@@ -58,20 +61,22 @@ def ects_requirements(current_courses, general_1, general_2, techno):
         if total >= needed:
             print(f"The requirements for {name} have been satisfied")
     print()
-    
-    print(f"The total number of ECTS points in the plan is {sum(current_courses.points) - 10}\n")  # Subtract 10 from total since these points are from the bachelor
 
-    plt.bar(list(range(len(totals)-1)), [totals[0] + totals[1], totals[2], totals[3]], color = ["red", "purple", "orange"])
-    plt.axhline(30, color = "k", linestyle = "--")
+    print(f"The total number of ECTS points in the plan is {
+        (current_courses.points) - 10}\
+            n")  # Subtract 10 from total since these points are from the bachelor
+
+    plt.bar(list(range(len(totals)-1)), [totals[0] + totals[1], totals[2], totals[3]], color=["red", "purple", "orange"])
+    plt.axhline(30, color="k", linestyle="--")
     plt.show()
 
 
-def alternative_courses(current, core, courselist):
+def alternative_courses(core, courselist):
     """Find all courses that have not been taken from the core and courselist from the data science requirements"""
 
     # Remove all courses that are already being taken to show alternatives
-    core_commons = pd.merge(current_courses, core["title"].reset_index(), how ='inner').set_index('index')
-    courselist_commons = pd.merge(current_courses, courselist["title"].reset_index(), how ='inner').set_index('index')
+    core_commons = pd.merge(current_courses, core["title"].reset_index(), how='inner').set_index('index')
+    courselist_commons = pd.merge(current_courses, courselist["title"].reset_index(), how='inner').set_index('index')
     core_alts = core.drop(core_commons.index.values.tolist())
     courselist_alts = courselist.drop(courselist_commons.index.values.tolist())
 
@@ -84,12 +89,13 @@ def alternative_courses(current, core, courselist):
     print("Alternative course from courselist:")
     print(courselist_alts, "\n")
 
+
 if __name__ == '__main__':
-    
-    current_courses = pd.read_csv("current_courses.csv", sep='\t', dtype={0:str, 1:str, 2:str, 3:float})
-    general_1 = pd.read_csv("master_requirements\general_competence_1.csv", sep='\t', dtype={0:str, 1:str, 2:float, 3:str})
-    general_2 = pd.read_csv("master_requirements\general_competence_2.csv", sep='\t', dtype={0:str, 1:str, 2:float, 3:str})
-    techno = pd.read_csv("master_requirements/technological_specialization.csv", sep='\t', dtype={0:str, 1:str, 2:float, 3:str})
+
+    current_courses = pd.read_csv("current_courses.csv", sep='\t', dtype={0: str, 1: str, 2: str, 3: float})
+    general_1 = pd.read_csv("master_requirements\general_competence_1.csv", sep='\t', dtype={0: str, 1: str, 2: float, 3: str})
+    general_2 = pd.read_csv("master_requirements\general_competence_2.csv", sep='\t', dtype={0: str, 1: str, 2: float, 3: str})
+    techno = pd.read_csv("master_requirements/technological_specialization.csv", sep='\t', dtype={0: str, 1: str, 2: float, 3: str})
 
     # print(general_1)
     # ects_above_required(current_courses, core, courselist)
