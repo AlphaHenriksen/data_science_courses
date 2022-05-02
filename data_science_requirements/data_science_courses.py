6b04afd9d9fef0cbfd1e57a7bc0311988d92a037
Anders <anders-10@live.dk>
2022-05-02 10:41:14 +0200

    general code cleanup and niceness

--- data_science_requirements/data_science_courses.py
+++ data_science_requirements/data_science_courses.py
@@ -26,8 +26,7 @@ timetable_to_time = {'January': "January",
                      'F2B': "Thu 8-12",
                      'F1B': "Thu 13-17",
                      'F4B': "Fri 8-12",
-                     'F3B': "Fri 13-17",
-}
+                     'F3B': "Fri 13-17"}
 
 
 def ects_above_required(courses, core, courselist):
@@ -35,8 +34,8 @@ def ects_above_required(courses, core, courselist):
     https://www.compute.dtu.dk/english/education/Data-Science-Big-Data/Core
     https://www.compute.dtu.dk/english/education/Data-Science-Big-Data/Courselist"""
 
-    core_commons = pd.merge(current_courses, core["title"], how ='inner')
-    courselist_commons = pd.merge(current_courses, courselist["title"], how ='inner')
+    core_commons = pd.merge(current_courses, core["title"], how='inner')
+    courselist_commons = pd.merge(current_courses, courselist["title"], how='inner')
 
     print("Total courses taken from core: ")
     print(core_commons, "\n")
@@ -58,8 +57,8 @@ def alternative_courses(current, core, courselist):
     """Find all courses that have not been taken from the core and courselist from the data science requirements"""
 
     # Remove all courses that are already being taken to show alternatives
-    core_commons = pd.merge(current_courses, core["title"].reset_index(), how ='inner').set_index('index')
-    courselist_commons = pd.merge(current_courses, courselist["title"].reset_index(), how ='inner').set_index('index')
+    core_commons = pd.merge(current_courses, core["title"].reset_index(), how='inner').set_index('index')
+    courselist_commons = pd.merge(current_courses, courselist["title"].reset_index(), how='inner').set_index('index')
     core_alts = core.drop(core_commons.index.values.tolist())
     courselist_alts = courselist.drop(courselist_commons.index.values.tolist())
 
@@ -67,18 +66,16 @@ def alternative_courses(current, core, courselist):
     core_alts['timetable_group'].replace(timetable_to_time, inplace=True)
     courselist_alts['timetable_group'].replace(timetable_to_time, inplace=True)
 
-    print("Alternative course from core:")
-    print(core_alts, "\n")
-    print("Alternative course from courselist:")
-    print(courselist_alts, "\n")
+    print("Alternative course from core:", core_alts, sep='\n', end='\n\n')
+    print("Alternative course from courselist:", courselist_alts, sep='\n', end='\n\n')
+
 
 if __name__ == '__main__':
-    
-    current_courses = pd.read_csv("current_courses.csv", sep='\t', dtype={0:str, 1:str, 2:str, 3:float})
-    core = pd.read_csv("data_science_requirements/core.csv", sep='\t', dtype={0:str, 1:str, 2:float, 3:str})
-    courselist = pd.read_csv("data_science_requirements/courselist.csv", sep='\t', dtype={0:str, 1:str, 2:float, 3:str})
 
-    
+    current_courses = pd.read_csv("current_courses.csv", sep='\t', dtype={0: str, 1: str, 2: str, 3: float})
+    core = pd.read_csv("data_science_requirements/core.csv", sep='\t', dtype={0: str, 1: str, 2: float, 3: str})
+    courselist = pd.read_csv("data_science_requirements/courselist.csv", sep='\t', dtype={0: str, 1: str, 2: float, 3: str})
+
     # print(current_courses)
     ects_above_required(current_courses, core, courselist)
     # alternative_courses(current_courses, core, courselist)
